19466b1059988ad5a26ef0574793421969a94585
Richard Osborne <richard@pockit.com>
2016-06-13 13:38:59 +0100

    Put data into a separate file

--- scripts/resources.js
+++ scripts/resources.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
     };
 
     var viewModel = function(recordings) {
-        
+
         var self = this;
-        
+
         self.recordings = ko.observableArray(recordings);
 
         self.sortBy = ko.observable("Tune Name");
@@ -47,57 +47,15 @@ $(document).ready(function() {
 
         self.pageSize = ko.observable(50);
 
-
         self.changeSort("Tune name");
     };
 
 
+    var jsonUrl = "/resources.json"
 
-var recordings = [
-            {
-                displayName: "Varvinder Friska", 
-                year: 2015, 
-                category: "Recording", 
-                tuneType: "Polska", 
-                workshop: "Swedish Tunes", 
-                source: "https://drive.google.com/open?id=0B36gCreiztaXR1JfcXVTenhMYVE"
-            },
-            {
-                displayName: "DOOM DOOM DOOM nananananananana", 
-                year: 2015, 
-                category: "Recording", 
-                tuneType: "Slangpolska", 
-                workshop: "Swedish Tunes", 
-                source: "https://drive.google.com/open?id=0B36gCreiztaXR1JfcXVTenhMYVE"
-            },
-            {
-                displayName: "DOOM DOOM DOOM nananananananana", 
-                year: 2015, 
-                category: "Sheet Music", 
-                tuneType: "Slangpolska", 
-                workshop: "Swedish Tunes", 
-                source: "https://drive.google.com/open?id=0B36gCreiztaXR1JfcXVTenhMYVE"
-            },
-            {
-                displayName: "Vals",
-                year: 2013, 
-                category: "Recording", 
-                tuneType: "Vals", 
-                workshop: "Swedish Tunes",
-                source:"https://drive.google.com/open?id=0B36gCreiztaXR1JfcXVTenhMYVE"
-            },
-            {
-                displayName: "Pols efter Hans Gruber", 
-                year:2014, 
-                category: "Sheet Music",
-                tuneType: "Pols", 
-                workshop: "Swedish Tunes", 
-                source: "https://drive.google.com/open?id=0B36gCreiztaXR1JfcXVTenhMYVE"
-            }
-            ];
-
-
-    ko.applyBindings(new viewModel(recordings));
+    $.getJSON(jsonUrl).done(function(data){
+        ko.applyBindings(new viewModel(data));
+    });
 
     viewModel.changeSort(viewModel.sortBy());
 
