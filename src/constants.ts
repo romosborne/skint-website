c51b7bb9621da2259df0fae865ee1dd05d950752
Ozzy <romosborne@gmail.com>
2025-05-01 09:18:28 +0100

    Set ticket releases to variables

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,7 @@ export const YEAR = '2025';
 
 export const FACEBOOK_EVENT_ID =
   'https://www.facebook.com/events/1119730636555625';
+
+export const FIRST_TICKET_RELEASE = '7pm UK time on Thursday 3rd July 2025';
+export const SECOND_TICKET_RELEASE = '7pm UK time on Thursday 9th October 2025';
+export const THIRD_TICKET_RELEASE = '7pm UK time on Thursday 30th October 2025';
