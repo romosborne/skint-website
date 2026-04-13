6d263a1acf2837f72bb96391e9a9386f2bd840ac
Ozzy <romosborne@gmail.com>
2026-04-13 20:24:27 +0100

    2026 Updates

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,15 @@
-export const REFUND_DATE = '8th October';
+export const REFUND_DATE = '29th October';
 
-export const YEAR = '2025';
+export const YEAR = '2026';
 
 export const FACEBOOK_EVENT_ID =
-  'https://www.facebook.com/events/1119730636555625';
+  'https://www.facebook.com/events/850218918091587';
 
-export const FIRST_TICKET_RELEASE = '7pm UK time on Thursday 3rd July 2025';
-export const SECOND_TICKET_RELEASE = '7pm UK time on Thursday 9th October 2025';
-export const THIRD_TICKET_RELEASE = '7pm UK time on Thursday 30th October 2025';
+export const FIRST_TICKET_RELEASE = '7pm UK time on Wednesday 1st July 2026';
+export const SECOND_TICKET_RELEASE =
+  '7pm UK time on Wednesday 7th October 2026';
+export const THIRD_TICKET_RELEASE = '7pm UK time on Friday 30th October 2026';
 
 export const TICKET_BOOKING_LINK =
-  'https://www.ticketsource.co.uk/booking/t-jzgpppq';
-export const TICKET_BOOKING_LIVE = false;
+  'https://www.ticketsource.com/booking/t-oeedqqp';
+export const TICKET_BOOKING_LIVE = true;
